4591fdaba9aeeb93bf7f4b736928d311f04d1016
Derek Olsen <derek.olsen@jivesoftware.com>
2014-10-22 11:20:48 -0700

    Added example config entry

--- spec/lib/flapjack/gateways/jabber_spec.rb
+++ spec/lib/flapjack/gateways/jabber_spec.rb
@@ -62,7 +62,6 @@ describe Flapjack::Gateways::Jabber, :logger => true do
     expect(EventMachine::Synchrony).to receive(:next_tick).and_yield
     expect(fj).to receive(:write).with(an_instance_of(Blather::Stanza::Presence))
     expect(fj).to receive(:write).with(an_instance_of(Blather::Stanza::Message))
-    fj.on_ready(stanza)
   end
 
   it "does not announce arrival in chat room after connecting" do
